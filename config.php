1f5a2ae7aabdb68ee5ab43c784d8a4ea273e2a9e
Ramzi-444 <ramzi.abdulalishoev_2026@ucentralasia.org>
2025-03-10 01:06:20 +0000

    Added Nextcloud configuration files

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc4n2sx0ilmp',
+  'passwordsalt' => 'uYkpZIa5864KF7hu4ie5fc+ccnylJR',
+  'secret' => 'ozAVF0lV4JkCW/5NwtF+y+VFJhEBNajvXqDDXY+loJ82q1aY',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.1.118',
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '31.0.0.18',
+  'overwrite.cli.url' => 'http://192.168.1.118',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextclouduser',
+  'dbpassword' => 'yourpassword',
+  'installed' => true,
+  'maintenance' => false,
+);
